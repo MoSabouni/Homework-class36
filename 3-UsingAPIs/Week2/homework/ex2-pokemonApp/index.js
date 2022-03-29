a60247858a3e4bbeeb8f2b770fc7fe068f44d438
Mo Sabouni <mas_95_@hotmail.com>
2022-03-29 14:07:59 +0200

    ex-2 is solved

--- 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
+++ 3-UsingAPIs/Week2/homework/ex2-pokemonApp/index.js
@@ -1,4 +1,5 @@
 'use strict';
+
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/blob/main/3-UsingAPIs/Week2/README.md#exercise-2-gotta-catch-em-all
 
@@ -22,18 +23,70 @@ Use async/await and try/catch to handle promises.
 Try and avoid using global variables. As much as possible, try and use function 
 parameters and return values to pass data back and forth.
 ------------------------------------------------------------------------------*/
-function fetchData(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchData(url) {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+    throw new Error(`Request failed.`);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
 
-function fetchAndPopulatePokemons(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchAndPopulatePokemons(data) {
+  const button = document.createElement('button');
+  button.classList.add('button');
+  button.textContent = 'Get Pokemon!';
+  button.type = 'button';
+  document.body.appendChild(button);
+
+  const selectElement = document.createElement('select');
+  selectElement.classList.add('selectElement');
+  document.body.appendChild(selectElement);
+  button.addEventListener('click', () => {
+    const pokemonResults = data.results;
+    pokemonResults.forEach((pokemon) => {
+      const option = document.createElement('option');
+      option.value = pokemon.name;
+      option.text = pokemon.name;
+      selectElement.appendChild(option);
+    });
+  });
+  selectElement.addEventListener('change', fetchImage);
 }
 
-function fetchImage(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+async function fetchImage(event) {
+  const previousImg = document.querySelector('img');
+  if (previousImg) {
+    previousImg.remove();
+  }
+  const selectedPokemon = `${event.currentTarget.value}`;
+  const url = `https://pokeapi.co/api/v2/pokemon/${selectedPokemon}`;
+  try {
+    const data = await fetchData(url);
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('imgElement');
+    imgElement.alt = 'Picture of Pokemon!';
+    imgElement.src = data.sprites.front_default;
+    document.body.appendChild(imgElement);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-function main() {
-  // TODO complete this function
+async function main() {
+  try {
+    const data = await fetchData(
+      `https://pokeapi.co/api/v2/pokemon/?limit=30&offset=30`
+    );
+    fetchAndPopulatePokemons(data);
+  } catch (error) {
+    console.log(error.message);
+  }
 }
+
+document.addEventListener('load', main());
